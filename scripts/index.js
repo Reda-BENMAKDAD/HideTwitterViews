effa4dc51f0d16536b7dc3129209caa16ed45c92
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-04-25 14:40:33 +0100

    added explanatory comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
+// this is the scripts that will be injected and run on the page
+// because i referred to it in the manifest.json in the content_scripts section
 setInterval(()=>{
+    // this selects all the elements with the label "Views. View Tweet analytics"
     views_things = document.querySelectorAll('[aria-label*="Views. View Tweet analytics"]');
     for(let i=0; i<views_things.length; i++) {
+        // and then removes them
         views_things[i].remove();
     }
 }, 1000);
